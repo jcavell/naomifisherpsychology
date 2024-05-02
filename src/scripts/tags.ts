c5a582c53dea7054d61d05c054b2992c8d5444cd
Jonny Cavell <jonny.cavell@gmail.com>
2024-05-02 21:41:36 +0100

    Lazy image loading and related courses

--- src/scripts/tags.ts
+++ src/scripts/tags.ts
@@ -25,11 +25,6 @@ const doIt = function (contentToCheck: string) {
     ["helping your", "parenting"].join("|"),
     "i"
   );
-  if (contentToCheck.includes("Low Demand Parenting")) {
-    tags.push("Low Demand Parenting");
-  } else {
-    if (parentingRegex.test(contentToCheck)) tags.push("Parenting");
-  }
 
   const pdaRegex = new RegExp(
     ["pda", "demand avoid", "pressure"].join("|"),
@@ -39,32 +34,51 @@ const doIt = function (contentToCheck: string) {
     ["autism", "autistic", "neurodivergent"].join("|"),
     "i"
   );
+
   const anxietyRegex = new RegExp(["anxiety", "anxious"].join("|"), "i");
   const traumaRegex = new RegExp(["trauma"].join("|"), "i");
   const screensRegex = new RegExp(["screen", "phone"].join("|"), "i");
   const aggressionRegex = new RegExp(["aggress", "violent"].join("|"), "i");
   const burnoutRegex = new RegExp(["burnout", "burnt out"].join("|"), "i");
+  const forTeenRegex = new RegExp(["for teen", "for adolesc"].join("|"), "i");
   const teenRegex = new RegExp(["teen", "adolesc"].join("|"), "i");
   const ehcpRegex = new RegExp(["ehcp"].join("|"), "i");
+
   const wellbeingRegex = new RegExp(
     ["Looking after yourself", "Look after yourself", "wellbeing"].join("|"),
     "i"
   );
+
   const writingRegex = new RegExp(["writing"].join("|"), "i");
   const schoolRegex = new RegExp(["school", "academic", "exam"].join("|"), "i");
 
-  if (pdaRegex.test(contentToCheck)) tags.push("Demand Avoidance");
-  if (autismRegex.test(contentToCheck)) tags.push("Autism");
-  if (anxietyRegex.test(contentToCheck)) tags.push("Anxiety");
-  if (traumaRegex.test(contentToCheck)) tags.push("Trauma");
-  if (burnoutRegex.test(contentToCheck)) tags.push("Burnout");
-  if (teenRegex.test(contentToCheck)) tags.push("Teenager");
+
+  if (forTeenRegex.test(contentToCheck)) tags.push("For Teenagers");
+
+  if (contentToCheck.includes("Low Demand Parenting")) {
+    tags.push("Low Demand Parenting");
+  } 
   if (ehcpRegex.test(contentToCheck)) tags.push("EHCP");
-  if (wellbeingRegex.test(contentToCheck)) tags.push("Wellbeing");
-//   if (writingRegex.test(contentToCheck)) tags.push("Writing");
-  if (schoolRegex.test(contentToCheck)) tags.push("School");
   if (screensRegex.test(contentToCheck)) tags.push("Screens");
   if (aggressionRegex.test(contentToCheck)) tags.push("Aggressive behaviour");
+
+ 
+  if (!forTeenRegex.test(contentToCheck) && teenRegex.test(contentToCheck)) tags.push("Teenager");
+
+  if (pdaRegex.test(contentToCheck)) tags.push("Demand Avoidance");
+  if (autismRegex.test(contentToCheck)) tags.push("Autism");
+  if (schoolRegex.test(contentToCheck)) tags.push("School");
+  if (traumaRegex.test(contentToCheck)) tags.push("Trauma");
+  if (burnoutRegex.test(contentToCheck)) tags.push("Burnout");
+  if (anxietyRegex.test(contentToCheck)) tags.push("Anxiety");
+
+
+  if (wellbeingRegex.test(contentToCheck)) tags.push("Wellbeing");
+//   if (writingRegex.test(contentToCheck)) tags.push("Writing");
+
+  if (!contentToCheck.includes("Low Demand Parenting")
+    && parentingRegex.test(contentToCheck))
+      tags.push("Parenting");
 
   return tags;
 };
