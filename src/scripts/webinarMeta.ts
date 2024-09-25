70e9649202e8790c5144f822bd0f52d846fcebe3
Jonny Cavell <jonny.cavell@gmail.com>
2024-09-25 16:54:21 +0100

    Add new webinar images

--- src/scripts/webinarMeta.ts
+++ src/scripts/webinarMeta.ts
@@ -14,6 +14,8 @@ import { naomi, abi, eliza, kate } from "./people";
 
 // Webinar Images
 import eotasImage from "../images/webinars/eotas-what-to-expect.jpg";
+import myFaultImage from "../images/webinars/is-it-all-my-fault.jpg";
+import transitionsImage from "../images/webinars/helping-your-autistic-child-with-transitions.jpg";
 
 type WebinarMeta = {
   id: string;
@@ -26,9 +28,17 @@ type WebinarMeta = {
 const is_it_all_my_fault: WebinarMeta = {
   id: "1008493379847",
   tags: [tagALDP, age5To11, age11To19, forParents],
+  image: myFaultImage,
   subsubtitle:
     "In an exclusive special offer, we are offering a free ticket for this webinar to anyone who preorders our book before Oct 9th. Click Learn More for details.",
   contributers: [naomi, eliza],
+};
+
+const helping_your_autistic_child_with_transitions: WebinarMeta = {
+  id: "888386577167",
+  tags: [tagAutism, tagAnxiety, age5To11, age11To19, forParents],
+  image: transitionsImage,
+  contributers: [naomi],
 };
 
 const eotas_what_to_expect: WebinarMeta = {
@@ -47,6 +57,7 @@ const writing_for_wellbeing: WebinarMeta = {
 // !! ADD WEBINAR HERE !!
 export function getWebinarMeta(eventId: string): WebinarMeta | undefined {
   return [
+    helping_your_autistic_child_with_transitions,
     is_it_all_my_fault,
     eotas_what_to_expect,
     writing_for_wellbeing
