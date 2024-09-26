79a4b016da8c84cfc89dc85b0a7b453ead6e9bce
Jonny Cavell <jonny.cavell@gmail.com>
2024-09-26 14:31:14 +0100

    Add GA tag using partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
-
+import partytown from '@astrojs/partytown'
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -16,5 +16,9 @@ export default defineConfig({
   //     "Access-Control-Allow-Credentials" : "*"
   //     }
   // },
-  integrations: [preact(), sitemap()]
+  integrations: [preact(), sitemap(), partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+        }),]
 });
