ec5830db49d2a768f7e81367c87ae4c74f910c06
Jonny Cavell <jonny.cavell@gmail.com>
2025-03-10 11:11:54 +0000

    Revert to aae69c6 and add new webinars

--- src/scripts/webinarMeta.ts
+++ src/scripts/webinarMeta.ts
@@ -14,18 +14,13 @@ import {
   tagNeurodiversity,
   tagScreens,
   tagWellbeing,
+  tagTrauma,
 } from "./tags";
 
 import { naomi, abi, eliza, kate } from "./people";
 
 // Webinar Images
 import myFaultImage from "../images/webinars/is-it-all-my-fault.jpg";
-import helpingYourDemandAvoidantChildWithChangeImage from "../images/webinars/helping-your-demand-avoidant-child-with-change.webp";
-import dyslexiaAndReadingDifficultiesImage from "../images/webinars/dyslexia-and-reading-difficulties.webp";
-import lowPressureParentingImage from "../images/webinars/low-pressure-parenting.webp";
-import restoringYourBalanceImage from "../images/webinars/restoring-your-balance.webp";
-import whatNowImage from "../images/webinars/what-now.jpg";
-import helpingYourChildWithBurnoutImage from "../images/webinars/helping-your-child-with-burnout.jpeg";
 
 type WebinarMeta = {
   id: string;
@@ -50,38 +45,37 @@ const helping_your_neurodivergent_teen_recover_from_school: WebinarMeta = {
   contributers: [naomi],
 };
 
-const what_now_diagnosis: WebinarMeta = {
-  id: "1203174349869",
-  tags: [tagALDP, age5To11, age11To19, forParents],
-  image: whatNowImage,
-  contributers: [naomi, eliza],
-};
-
-const calm_and_connect: WebinarMeta = {
-  id: "1244751046889",
-  tags: [tagWellbeing, forParents],
-  contributers: [abi],
-};
-
-const my_child_hates_maths: WebinarMeta = {
-  id: "1244786462819",
-  tags: [tagSEN, tagSchool, age5To11, age11To19, forParents],
-  contributers: [abi],
-};
-
 const demand_avoidant_child: WebinarMeta = {
   id: "1246046932919",
   tags: [tagDemandAvoidance, forParents, age5To11, age11To19],
   contributers: [naomi],
 };
 
+const navigating_the_sen_tribunal_process: WebinarMeta = {
+  id: "1257579777969",
+  tags: [tagSEN, tagSchool, forParents, age5To11, age11To19],
+  contributers: [abi],
+};
+
+const now_what_not_fine_at_school: WebinarMeta = {
+  id: "1264018757129",
+  tags: [tagSchool, forParents, age5To11, age11To19],
+  contributers: [naomi, eliza],
+};
+
+const helping_your_autistic_child_with_trauma: WebinarMeta = {
+  id: "1267249911599",
+  tags: [tagAutism, tagTrauma, forParents, age5To11, age11To19],
+  contributers: [naomi],
+};
+
 // !! ADD WEBINAR HERE !!
 export function getWebinarMeta(eventId: string): WebinarMeta | undefined {
   return [
-    calm_and_connect,
     demand_avoidant_child,
     helping_your_neurodivergent_teen_recover_from_school,
-    my_child_hates_maths,
-    what_now_diagnosis,
+    navigating_the_sen_tribunal_process,
+    now_what_not_fine_at_school,
+    helping_your_autistic_child_with_trauma,
   ].find((webMeta) => eventId === webMeta.id);
 }
