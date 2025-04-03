8d59c976ac05c83db16bda86d902318339d5fc58
Jonny Cavell <jonny.cavell@gmail.com>
2025-04-03 13:31:56 +0100

    Add new webinar

--- src/scripts/webinarMeta.ts
+++ src/scripts/webinarMeta.ts
@@ -39,36 +39,6 @@ const example_webinar: WebinarMeta = {
   contributers: [naomi, eliza],
 };
 
-const demand_avoidant_child: WebinarMeta = {
-  id: "1246046932919",
-  tags: [tagDemandAvoidance, forParents, age5To11, age11To19],
-  contributers: [naomi],
-};
-
-const navigating_the_sen_tribunal_process: WebinarMeta = {
-  id: "1257579777969",
-  tags: [tagSEN, tagSchool, forParents, age5To11, age11To19],
-  contributers: [abi],
-};
-
-const now_what_not_fine_at_school: WebinarMeta = {
-  id: "1264018757129",
-  tags: [tagSchool, forParents, age5To11, age11To19],
-  contributers: [naomi, eliza],
-};
-
-const helping_your_autistic_child_with_trauma: WebinarMeta = {
-  id: "1267249911599",
-  tags: [tagAutism, tagTrauma, forParents, age5To11, age11To19],
-  contributers: [naomi],
-};
-
-const does_my_child_need_an_ehcp: WebinarMeta = {
-  id: "1284018246139",
-  tags: [tagSEN, tagEHCPs, tagSchool, forParents, age5To11, age11To19],
-  contributers: [abi],
-};
-
 const book_launch_school_not_working: WebinarMeta = {
   id: "1292582281399",
   tags: [tagSchool, forParents, age5To11, age11To19],
@@ -86,15 +56,17 @@ const understanding_demand_avoidant_teen: WebinarMeta = {
   contributers: [naomi],
 };
 
+const now_what_recovering_after_school_breakdown: WebinarMeta = {
+  id: "1311499834309",
+  tags: [tagSchool, forParents, age5To11, age11To19],
+  contributers: [naomi, eliza],
+};
+
 // !! ADD WEBINAR HERE !!
 export function getWebinarMeta(eventId: string): WebinarMeta | undefined {
   return [
-    demand_avoidant_child,
-    navigating_the_sen_tribunal_process,
-    now_what_not_fine_at_school,
-    helping_your_autistic_child_with_trauma,
-    does_my_child_need_an_ehcp,
     book_launch_school_not_working,
+    now_what_recovering_after_school_breakdown,
     not_fine_at_school_kinship,
     understanding_demand_avoidant_teen,
   ].find((webMeta) => eventId === webMeta.id);
