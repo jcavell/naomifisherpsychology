3c2740ce54cd804be78748ac56cfa3ffb54c3e49
Jonny Cavell <jonny.cavell@gmail.com>
2026-01-26 08:13:43 +0000

    Revert "Autism course sale and new webinars"
    This reverts commit ba942afc0ee1b979f0aca23138b958f3c14ec50c.
    
    End sale

--- src/scripts/webinarMeta.ts
+++ src/scripts/webinarMeta.ts
@@ -22,6 +22,12 @@ import { naomi, abi, eliza, kate } from "./people";
 
 // EventbriteWebinar Images
 import demandAvoidantChildImage from "../images/webinars/understanding-and-helping-your-demand-avoidand-child.avif";
+import schoolTraumaImage from "../images/webinars/school-trauma.jpg";
+import aldpChristmasImage from "../images/webinars/aldp-christmas.jpg";
+import writingForWellbeingImage from "../images/webinars/writing-for-wellbeing.jpg";
+import dyslexiaImage from "../images/webinars/dyslexia.jpg";
+import demandAvoidantChangeImage from "../images/webinars/demand-avoidant-change.jpg";
+import autisticAngerImage from "../images/webinars/autistic-anger.jpg";
 
 
 type WebinarMeta = {
@@ -42,27 +48,52 @@ const example_webinar: WebinarMeta = {
 };
 
 
-const notFineAtSchool: WebinarMeta = {
-  id: "1980544344449",
-  tags: [tagSchool, age5To11, age11To19, forParents],
+const schoolTrauma: WebinarMeta = {
+  id: "1828642532039",
+  tags: [tagTrauma, tagSchool, age5To11, age11To19, forParents],
+  image: schoolTraumaImage,
   contributers: [naomi],
 };
 
-const ehcpBookLaunch: WebinarMeta = {
-  id: "1980668709428",
-  tags: [tagEHCPs, tagSEN, tagSchool, age5To11, age11To19, forParents],
-  contributers: [abi, eliza],
+const writingForWellbeing: WebinarMeta = {
+  id: "1870305446939",
+  tags: [tagWellbeing, forParents],
+  image: writingForWellbeingImage,
+  contributers: [abi],
 };
 
-  const burntOutByParenting: WebinarMeta = {
-    id: "1980545402614",
-    tags: [tagBurnout, forParents],
-    contributers: [naomi],
-  };
+const dyslexia: WebinarMeta = {
+  id: "1837990482009",
+  tags: [tagSEN, tagSchool, age5To11, age11To19, forParents],
+  image: dyslexiaImage,
+  contributers: [abi],
+};
+
+const demandAvoidantChange: WebinarMeta = {
+  id: "1964030267400",
+  tags: [tagDemandAvoidance, age5To11, age11To19, forParents],
+  image: demandAvoidantChangeImage,
+  contributers: [naomi],
+};
+
+const autisticAnger: WebinarMeta = {
+  id: "1964043054647",
+  tags: [tagAutism, age5To11, age11To19, forParents],
+  image: autisticAngerImage,
+  contributers: [naomi],
+};
+
+const aldpChristmas: WebinarMeta = {
+  id: "1965441059116",
+  tags: [tagALDP, tagDemandAvoidance, age5To11, age11To19, forParents],
+  image: aldpChristmasImage,
+  contributers: [naomi, eliza],
+};
+
 
 // !! ADD WEBINAR HERE !!
 export function getWebinarMeta(eventId: string): WebinarMeta | undefined {
-  return [notFineAtSchool, ehcpBookLaunch, burntOutByParenting].find(
+  return [schoolTrauma, writingForWellbeing, dyslexia, demandAvoidantChange, autisticAnger, aldpChristmas].find(
     (webMeta) => eventId === webMeta.id,
   );
 }
