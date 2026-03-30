1fb0b90667738c40f99aa27abeeed8870889260e
Jonny Cavell <jonny.cavell@gmail.com>
2026-03-30 09:07:04 +0100

    Revert "School course sale"
    This reverts commit b04f66c0970e76640d608d682295f26fd9009ee9.

--- src/scripts/webinarMeta.ts
+++ src/scripts/webinarMeta.ts
@@ -23,7 +23,7 @@ import { naomi, abi, eliza, kate } from "./people";
 // EventbriteWebinar Images
 import demandAvoidantChildImage from "../images/webinars/understanding-and-helping-your-demand-avoidand-child.avif";
 import rejectionSensitivityImage from "../images/webinars/helping-your-child-with-rejection-sensitivity.jpg";
-import autisticPTSDImage from "../images/webinars/autistic-ptsd.webp";
+import autisticChildAnxietyImage from "../images/webinars/helping-your-autistic-child-with-anxiety.png";
 
 
 type WebinarMeta = {
@@ -44,16 +44,23 @@ const example_webinar: WebinarMeta = {
 };
 
 
-const autisticPTSD: WebinarMeta = {
-  id: "1985569311260",
-  tags: [tagAutism, tagNeurodiversity, tagTrauma, age5To11, age11To19, forParents],
-  image: autisticPTSDImage,
+const rejectionSensitivity: WebinarMeta = {
+  id: "1982283058991",
+  tags: [tagNeurodiversity, age5To11, age11To19, forParents],
+  image: rejectionSensitivityImage,
+  contributers: [naomi],
+};
+
+const autisticAnxiety: WebinarMeta = {
+  id: "1982284000808",
+  tags: [tagAutism, tagAnxiety, age5To11, age11To19, forParents],
+  image:autisticChildAnxietyImage,
   contributers: [naomi],
 };
 
 // !! ADD WEBINAR HERE !!
 export function getWebinarMeta(eventId: string): WebinarMeta | undefined {
-  return [autisticPTSD].find(
+  return [rejectionSensitivity, autisticAnxiety].find(
     (webMeta) => eventId === webMeta.id,
   );
 }
