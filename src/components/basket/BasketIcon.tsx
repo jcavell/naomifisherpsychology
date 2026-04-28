4d0cf350999d419d8e1746e56afd7be2ff845b26
Jonny Cavell <jonny.cavell@gmail.com>
2026-04-28 10:08:41 +0100

    Fix hydration error and error referencing old deleted component

--- src/components/basket/BasketIcon.tsx
+++ src/components/basket/BasketIcon.tsx
@@ -1,38 +1,35 @@
-import React from "react";
-import { useStore } from '@nanostores/react';
-import { getItemCount } from '../../scripts/basket/basket.ts';
+import React, {useEffect, useState} from "react";
+import {useStore} from '@nanostores/react';
+import {getItemCount} from '../../scripts/basket/basket.ts';
 import styles from "../../styles/components/cart/basketIcon.module.css";
 
 const BasketIcon: React.FC = () => {
-    const count = useStore(getItemCount);
+    const $count = useStore(getItemCount);
 
-    // 🚫 Don't render anything if empty
-    if (!count || count <= 0) {
-        return null;
-    }
+    const [clientTotalItems, setClientTotalItems] = useState(0);
+
+    useEffect(() => {
+        setClientTotalItems($count);
+    }, [$count]);
+
+  if (clientTotalItems <= 0) {
+    return null;
+  }
 
     return (
-      <a href="/basket" className={`basket-button ${styles.basketWrapper}`}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-              <circle cx="9" cy="21" r="1" />
-              <circle cx="20" cy="21" r="1" />
-              <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
-          </svg>
-
-          <span className={styles.basketCount}>
-                {count}
-            </span>
-      </a>
+        <a href="/basket" className={`basket-button ${styles.basketWrapper}`}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <circle cx="9" cy="21" r="1"></circle>
+                <circle cx="20" cy="21" r="1"></circle>
+                <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+            </svg>
+            {clientTotalItems > 0 && (
+                <span className={styles.basketCount}>
+          {clientTotalItems}
+        </span>
+            )}
+        </a>
     );
 };
 
